89e8e5f051dec4bc0ca58f22fe4a7c123852eb28
auhppy <91692012+auliaahc@users.noreply.github.com>
2023-06-06 16:08:34 +0700

    update

--- app/src/main/java/com/example/projectakhirpam/ProfileActivity.java
+++ app/src/main/java/com/example/projectakhirpam/ProfileActivity.java
@@ -14,9 +14,12 @@ import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -55,12 +58,12 @@ public class ProfileActivity extends AppCompatActivity {
             }
         });
 
-//        editProfile.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                passUserData();
-//            }
-//        });
+        editProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                passUserData();
+            }
+        });
     }
 
     public void showAllUserData(){
@@ -74,31 +77,32 @@ public class ProfileActivity extends AppCompatActivity {
         fullName.setText(dataFullname);
         phoneNumber.setText(dataPhoneNumber);
         emailLogin.setText(dataEmail);
-
     }
-//    public void passUserData(){
-//        String userUsername = profileUsername.getText().toString().trim();
-//        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("users");
-//        Query checkUserDatabase = reference.orderByChild("username").equalTo(userUsername);
-//        checkUserDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
-//            @Override
-//            public void onDataChange(@NonNull DataSnapshot snapshot) {
-//                if (snapshot.exists()){
-//                    String nameFromDB = snapshot.child(userUsername).child("name").getValue(String.class);
-//                    String emailFromDB = snapshot.child(userUsername).child("email").getValue(String.class);
-//                    String usernameFromDB = snapshot.child(userUsername).child("username").getValue(String.class);
-//                    String passwordFromDB = snapshot.child(userUsername).child("password").getValue(String.class);
-//                    Intent intent = new Intent(ProfileActivity.this, EditProfileActivity.class);
-//                    intent.putExtra("name", nameFromDB);
-//                    intent.putExtra("email", emailFromDB);
-//                    intent.putExtra("username", usernameFromDB);
-//                    intent.putExtra("password", passwordFromDB);
-//                    startActivity(intent);
-//                }
-//            }
-//            @Override
-//            public void onCancelled(@NonNull DatabaseError error) {
-//            }
-//        });
-//    }
+
+    public void passUserData(){
+        String userUsername = username.getText().toString().trim();
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Users");
+        Query checkUserDatabase = reference.orderByKey().equalTo(user.getUid());
+        checkUserDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()){
+                    String usernameFromDB = snapshot.child(userUsername).child("email").getValue(String.class);
+                    String fullnameFromDB = snapshot.child(userUsername).child("name").getValue(String.class);
+                    String phonenumberFromDB = snapshot.child(userUsername).child("phoneNUmber").getValue(String.class);
+                    String emailFromDB = snapshot.child(userUsername).child("email").getValue(String.class);
+                    Intent intent = new Intent(ProfileActivity.this, EditProfileActivity.class);
+
+                    intent.putExtra("name", usernameFromDB);
+                    intent.putExtra("email", fullnameFromDB);
+                    intent.putExtra("username", phonenumberFromDB);
+                    intent.putExtra("password", emailFromDB);
+                    startActivity(intent);
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
+    }
 }
