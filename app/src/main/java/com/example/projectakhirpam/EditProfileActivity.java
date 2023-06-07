504ff55ce6f6318fadc87ace862efe421da4b20e
ardhiwahyuuu <ardhiwahyu@student.ub.ac.id>
2023-06-07 15:45:16 +0700

    add file

--- app/src/main/java/com/example/projectakhirpam/EditProfileActivity.java
+++ app/src/main/java/com/example/projectakhirpam/EditProfileActivity.java
@@ -3,7 +3,7 @@ package com.example.projectakhirpam;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class EditProfil extends AppCompatActivity {
+public class EditProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profil);
